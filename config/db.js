f14ad208e09948f5b776b7272a573871a7a6d2be
rami-mohamad <rami_zezo1986@hotmail.com>
2026-03-09 09:45:43 +0100

    fixed the Mongodb uri

--- config/db.js
+++ config/db.js
@@ -8,12 +8,7 @@ module.exports = async function connectDB() {
 
     console.log("MONGO_DB_USER:", process.env.MONGO_URI);
 
-    // const uri =
-    //   process.env.MONGO_URI ||
-    //   `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
-
-    const uri =
-      "mongodb+srv://kik2gether:6614380Ramifo!@cluster0.v4lyg.mongodb.net/kik2gether?retryWrites=true&w=majority";
+    const uri = process.env.MONGO_URI;
 
     mongoose.set("strictQuery", true);
 
